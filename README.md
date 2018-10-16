35a09e7302c474cb50e27b88c2327366084ae68d
Yushi Yang <yangyushi1992@icloud.com>
2018-10-16 18:20:06 +0100

    PEP8 code refactoring, better explained readme, ignore unnecessary files

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Introduction
 
-Written for first year BCFN students at the University of Bristol as part of the compuatational nanoscience course. 
+Written for first year BCFN students at the University of Bristol as part of the computational nanoscience course by Paddy Royall.
 
-This code is to be used in conjunction with: https://github.com/tranqui/monte_carlo .The code here is an aid for students still very new to programming in python and does not use any advanced python features and libraries. Students with prior experience of python may find the [this repository](https://github.com/tranqui/monte_carlo) more educational.
+The code here is an aid for students to understand basic Monte-Carlo simulation. It is written in a way that student without prior programming experiences could easily understand it. We deliberately avoided any advanced python features and libraries. Students with prior experience of python may find more pythonic code and tricks in the [repository from Josh](https://github.com/tranqui/monte_carlo), which performs the same Monte-Carlo simulation.
 
 ## What to Do
 
@@ -13,3 +13,4 @@ There are four steps towards a successful Monte-Carlo simulation  of hard sphere
 1. Simulating a random gas with the periodic boundary condition (PBC).
 2. Simulating repelling hard spheres with PBC.
 3. Simulating attracting hard spheres with PBC.
+4. Change the parameters and observe different behaviours.
--- ideal_gas.py
+++ ideal_gas.py
@@ -1,81 +1,81 @@
 """
-Ideal gas: This script creates a lattice of non-interacting particles 
+Ideal gas: This script creates a lattice of non-interacting particles
 to introduce the students to periodic boundary conditions.
 """
 import random as random
 import numpy as np
 
-#Initial parameters
-L = 5 #lattice unit cells per dimention
-a = 1 #lattice parameter
-box = L * a
-N = (L)**3 #number of particles
-Total_steps = 100
+# Initial parameters
+unit_repeat = 5  # number of unit cells per dimension
+lattice_constant = 1
+box_size = lattice_constant * unit_repeat
+particle_number = unit_repeat ** 3  # number of particles
+total_steps = 100
 diameter = 1
 volume_fraction = 0.1
 
+# rewrite file
+output_file = open('positions_random_gas.xyz', 'w')
+output_file.close()
 
-#rewrite file
-myfile = open('positions.txt' , 'w')
-myfile.close()
+# Generate initial co-ordinates
+x, y, z = [], [], []
+for i in range(0, unit_repeat):
+    for j in range(0, unit_repeat):
+        for k in range(0, unit_repeat):
+            x.append(i * lattice_constant)
+            y.append(j * lattice_constant)
+            z.append(k * lattice_constant)
 
-#Generate initial co-ordinates
-x=[]
-y=[]
-z=[]
-for i in range(0,L):
-    for j in range(0,L):
-        for k in range(0,L):
-            x.append(i*a)
-            y.append(j*a)
-            z.append(k*a) 
-
-            
 # Rescale the box/coordinates so we achieve the correct volume fraction/density.
 atoms_per_cell = 1
-atom_volume = np.pi*diameter**3 / 6
-initial_volume_fraction = atoms_per_cell * atom_volume # / unit volume (=1)
-rescale = (initial_volume_fraction/volume_fraction)**(1./3)
-box *= rescale 
-for a in range(0,N):
+atom_volume = np.pi * diameter ** 3 / 6
+initial_volume_fraction = atoms_per_cell * atom_volume  # / unit volume (=1)
+rescale = (initial_volume_fraction / volume_fraction) ** (1. / 3)
+box_size *= rescale
+
+for a in range(0, particle_number):
     x[a] *= rescale
-for a in range(0,N):
+for a in range(0, particle_number):
     y[a] *= rescale
-for a in range(0,N):
+for a in range(0, particle_number):
     z[a] *= rescale
 
-#Move particles        
-for t in range(0,Total_steps):   
-    
-    #Write positions to file
-    with open('positions.txt','a') as f:
+# Move particles, output their coordinates
+for t in range(0, total_steps):
+    # Write positions to file
+    with open('positions_random_gas.xyz', 'a') as f:
         f.write(str(len(x)) + '\n')
         f.write('\n')
         for i in range(len(x)):
+            # xyz file is a file format to store 3D position
+            # the general format is:
+            # PARTICLE_TYPE  X  Y  Z
+            # here we just call our random gas particles "1"
             f.write('1' + '\t' + str(x[i]) + '\t' + str(y[i]) + '\t' + str(z[i]) + '\n')
-                        
-    for i in range(0,N):        
-        #Trial Move
-        trial_x = x[i] + random.gauss(0,1)
-        trial_y = y[i] + random.gauss(0,1)
-        trial_z = z[i] + random.gauss(0,1)
-        
-        #Check boundaries       
-        if trial_x <= 0:
-            trial_x += box
-        elif trial_x >= box:
-            trial_x -= box    
-        
-        if trial_y <= 0:
-            trial_y += box
-        elif trial_y >= box:
-            trial_y -= box 
-            
-        if trial_z <= 0:
-            trial_z += box
-        elif trial_z >= box:
-            trial_z -= box
 
-        #Confirm movements
-        x[i],y[i],z[i] = trial_x,trial_y,trial_z       
-        
+    for i in range(0, particle_number):
+        # Trial Move
+        trial_x = x[i] + random.gauss(0, 1)
+        trial_y = y[i] + random.gauss(0, 1)
+        trial_z = z[i] + random.gauss(0, 1)
+
+        # Check boundaries
+        # We always move particles a small step, so don't worry if trial_x >> box_size
+        if trial_x <= 0:
+            trial_x += box_size
+        elif trial_x >= box_size:
+            trial_x -= box_size
+
+        if trial_y <= 0:
+            trial_y += box_size
+        elif trial_y >= box_size:
+            trial_y -= box_size
+
+        if trial_z <= 0:
+            trial_z += box_size
+        elif trial_z >= box_size:
+            trial_z -= box_size
+
+        # Confirm movements
+        x[i], y[i], z[i] = trial_x, trial_y, trial_z
